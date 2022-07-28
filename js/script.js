1591a89c7803becae754f88cbe7c4f9e67ad0f17
Cristian Medina <medinacristian@hotmail.com>
2022-07-28 21:02:40 +0200

    Add validation on form and add to library

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const books = document.querySelector('.books');
-const btnAddBook = document.querySelector('.btn-add-book');
+const btnShowForm = document.querySelector('.btn-show-form');
 const formAddBook = document.querySelector('.form');
+const btnAddBook = document.querySelector('.btn-add-book');
+const inputElements = document.querySelectorAll('input');
 
 let myLibrary = [
   { title: 'Blood of Elves', author: 'Andrzej Sapkowski', pages: '320', read: true },
@@ -28,10 +30,16 @@ function addBookToLibrary(obj) {
 function displayBooks(){
   let card, title, author, pages, read;
 
+  // Count books on page for knowing how many are missing
+  let countPresentedBooks = books.childElementCount === 0 ?
+    0 : 
+    myLibrary.length - (myLibrary.length - books.childElementCount);
+
   // Loop through the array "myLibrary" and 
   // create a card for each book with its data.
+  // If only some books are presented, add the ones missing.
   // Add the books to the page.
-  for (let i = 0; i < myLibrary.length; i++) {
+  for (let i = countPresentedBooks; i < myLibrary.length; i++) {
     // Create card
     card = document.createElement('div');
     card.classList.add('book');
@@ -62,10 +70,64 @@ function displayBooks(){
 }
 
 // Show and animate the form
-btnAddBook.addEventListener('click', () => {
+btnShowForm.addEventListener('click', () => {
   formAddBook.setAttribute('open', '')
   formAddBook.style.display = 'block';
-  btnAddBook.style.display = 'None';
+  btnShowForm.style.display = 'None';
+})
+
+// When click on button "add book"
+btnAddBook.addEventListener('click', (e) => {
+  // Prevent the submit when clicking the button
+  e.preventDefault();
+
+  // Get input from form
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('input[name="read"]:checked').id;
+  read = read === 'yes' ? true : false;
+
+  // If form field empty add error class
+  if (title === '' || author === '' || pages === '') {
+    if (title === '') document.querySelector('#title').classList.add('error');
+    if (author === '') document.querySelector('#author').classList.add('error');
+    if (pages === '') document.querySelector('#pages').classList.add('error');
+
+  } else {
+    // Use constructor to create a book
+    let newBook = new Book(title, author, pages, read);
+
+    // Add book to library
+    addBookToLibrary(newBook);
+
+    // Display the new book
+    displayBooks();
+
+    // Empty the form
+    formAddBook.reset();
+
+    // Hide form
+    formAddBook.setAttribute('close', '')
+
+    formAddBook.addEventListener("animationend", () => {
+      formAddBook.style.display = 'none';
+      formAddBook.removeAttribute('close', '')
+
+      // Show button to add new book
+      btnShowForm.style.display = 'block';
+    }, { once: true });
+    
+  }
+})
+
+// Remove error class from form
+inputElements.forEach((input) => {
+  input.addEventListener('focusout', () => {
+    if (input.classList[0] === 'error' && input.value !== '') {
+      input.classList.remove('error');
+    }
+  })
 })
 
 displayBooks();
