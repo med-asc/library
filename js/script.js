27f75bf98e08459552600aacb249c570176152e9
Cristian Medina <medinacristian@hotmail.com>
2022-07-29 10:27:29 +0200

    Add a delete button for each book

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const btnShowForm = document.querySelector('.btn-show-form');
 const formAddBook = document.querySelector('.form');
 const btnAddBook = document.querySelector('.btn-add-book');
 const inputElements = document.querySelectorAll('input');
+let bookIdCounter = 0;
 
 let myLibrary = [
   { title: 'Blood of Elves', author: 'Andrzej Sapkowski', pages: '320', read: true },
@@ -28,7 +29,7 @@ function addBookToLibrary(obj) {
 }
 
 function displayBooks(){
-  let card, title, author, pages, read;
+  let card, cardHeading, cardContent, cardFooter, title, author, pages, read, delBtn;
 
   // Count books on page for knowing how many are missing
   let countPresentedBooks = books.childElementCount === 0 ?
@@ -43,6 +44,20 @@ function displayBooks(){
     // Create card
     card = document.createElement('div');
     card.classList.add('book');
+    bookIdCounter++
+    card.setAttribute('data-index', bookIdCounter);
+
+    // Create Card heading div
+    cardHeading = document.createElement('div');
+    cardHeading.classList.add('card-heading')
+
+    // Create Card content div
+    cardContent = document.createElement('div');
+    cardContent.classList.add('card-content')
+
+    // Create Card footer div
+    cardFooter = document.createElement('div');
+    cardFooter.classList.add('card-footer')
 
     // Create title
     title = document.createElement('h2');
@@ -60,13 +75,39 @@ function displayBooks(){
     read = document.createElement('p');
     read.textContent = `${myLibrary[i].read ? 'I have read this book.' : 'I have not read this book yet.'}`;
 
+    // Create delet btn
+    delBtn = document.createElement('button');
+    delBtn.textContent = 'Delete';
+    delBtn.classList.add('btn', 'btn-red');
+
     // Append elements
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
+    cardHeading.appendChild(title);
+    card.appendChild(cardHeading);
+
+    cardContent.append(author, pages, read);
+    card.appendChild(cardContent);
+
+    cardFooter.appendChild(delBtn)
+    card.appendChild(cardFooter);
+
     books.appendChild(card);
   }
+
+  // Delete function
+  // If click on delete button, grab
+  // data-index attribute and delete that index in array
+  const btnDel = document.querySelectorAll('.btn-red')
+  btnDel.forEach((button) => {
+    button.addEventListener('click', () => {
+      let bookId = button.parentElement.parentElement.getAttribute('data-index');
+      let bookIndex = myLibrary.findIndex(object => {
+        return object.id === bookId;
+      });
+  
+      myLibrary.splice(bookIndex, 1);
+      button.parentElement.parentElement.remove();
+    })
+  })
 }
 
 // Show and animate the form
